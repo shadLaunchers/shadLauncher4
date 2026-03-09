6353ff79e2fe6116abeb7aac748951a43631a336
georgemoralis <giorgosmrls@gmail.com>
2026-03-09 13:27:07 +0200

    [ci skip] Qt GUI: Update Translation. (#83)
    Co-authored-by: georgemoralis <4313123+georgemoralis@users.noreply.github.com>

--- src/qt_ui/translations/en_US.ts
+++ src/qt_ui/translations/en_US.ts
@@ -1365,6 +1365,22 @@ Using local database.
         <source>Failed to convert icon.</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>&amp;Launch game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Launch game with current settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Launch game with default settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Launch game with global settings</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>GameListGrid</name>
