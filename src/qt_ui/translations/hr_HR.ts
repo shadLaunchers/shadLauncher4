559fc782c26b4f7a4318fa811362db5b59f7c794
georgemoralis <giorgosmrls@gmail.com>
2026-04-05 07:54:35 +0300

    New translations en_us.ts (Croatian)

--- src/qt_ui/translations/hr_HR.ts
+++ src/qt_ui/translations/hr_HR.ts
@@ -1400,6 +1400,14 @@ sam izvještaj ne bi postao nevaljan:</translation>
       <source>&amp;Launch game with global settings</source>
       <translation>&amp;Pokreni igru uz globalne postavke</translation>
     </message>
+    <message>
+      <source>Trophy Viewer</source>
+      <translation type="unfinished">Trophy Viewer</translation>
+    </message>
+    <message>
+      <source>No games found. Please add your games to your library first.</source>
+      <translation type="unfinished">No games found. Please add your games to your library first.</translation>
+    </message>
   </context>
   <context>
     <name>GameListGrid</name>
@@ -3400,6 +3408,33 @@ To će ukloniti sve igri-specifične postavke.</translation>
     <message>
       <source>Readback Mode:\nControls how the emulator handles GPU readbacks.Enabled them with make some games work better.\nDisabled is recommended option\nRelaxed is same as Precised but uses less fence protection,but can give more fps\nPrecise mode should be used for maximum compatibility where readbacks are neccesary</source>
       <translation>Readbacks Mod:\nKontrolira kako emulator rukuje GPU povratne informacije. Omogućeno čini neke igre pokretljivijim.\nOnemogućeno je preporučeno.\nOpušteno je isto kao Precizno s manje zaštite, uz malo više fps-ova.\nPrecizni mod trebao bi se koristit za maksimalnu kompatibilnost gdje su readbackovi potrebni</translation>
+    </message>
+  </context>
+  <context>
+    <name>TrophyViewer</name>
+    <message>
+      <source>Progress</source>
+      <translation type="unfinished">Progress</translation>
+    </message>
+    <message>
+      <source>Trophy Viewer</source>
+      <translation type="unfinished">Trophy Viewer</translation>
+    </message>
+    <message>
+      <source>Select Game:</source>
+      <translation type="unfinished">Select Game:</translation>
+    </message>
+    <message>
+      <source>Show Earned Trophies</source>
+      <translation type="unfinished">Show Earned Trophies</translation>
+    </message>
+    <message>
+      <source>Show Not Earned Trophies</source>
+      <translation type="unfinished">Show Not Earned Trophies</translation>
+    </message>
+    <message>
+      <source>Show Hidden Trophies</source>
+      <translation type="unfinished">Show Hidden Trophies</translation>
     </message>
   </context>
   <context>
