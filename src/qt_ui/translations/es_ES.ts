428b473f3340b3effe4e019a9b617a7960fdbea5
georgemoralis <giorgosmrls@gmail.com>
2026-06-18 15:36:44 +0300

    New translations en_us.ts (Spanish)
    [ci skip]

--- src/qt_ui/translations/es_ES.ts
+++ src/qt_ui/translations/es_ES.ts
@@ -3276,10 +3276,6 @@ Selecciona una versión del emulador en el panel derecho.</translation>
       <translation>Habilitar modo PS4 Neo</translation>
     </message>
     <message>
-      <source>Set &quot;PSN signed-in&quot; to True</source>
-      <translation>Cambia la opción «Inicio de sesión en PSN» a verdadero</translation>
-    </message>
-    <message>
       <source>Set &quot;Network Connected&quot; to True</source>
       <translation>Cambia la opción «Conexión a red» a verdadero</translation>
     </message>
@@ -3589,6 +3585,46 @@ Se eliminarán todos los ajustes específicos para cada juego.</translation>
       <source>Default</source>
       <translation>Predeterminada</translation>
     </message>
+    <message>
+      <source>Use Circle Button to Confirm</source>
+      <translation type="unfinished">Use Circle Button to Confirm</translation>
+    </message>
+    <message>
+      <source>IME</source>
+      <translation type="unfinished">IME</translation>
+    </message>
+    <message>
+      <source>Enable IME Accessibility</source>
+      <translation type="unfinished">Enable IME Accessibility</translation>
+    </message>
+    <message>
+      <source>Enable IME URL/Email Short Panel</source>
+      <translation type="unfinished">Enable IME URL/Email Short Panel</translation>
+    </message>
+    <message>
+      <source>Enable ShadNet</source>
+      <translation type="unfinished">Enable ShadNet</translation>
+    </message>
+    <message>
+      <source>ShadNet</source>
+      <translation type="unfinished">ShadNet</translation>
+    </message>
+    <message>
+      <source>Server</source>
+      <translation type="unfinished">Server</translation>
+    </message>
+    <message>
+      <source>Signaling Address</source>
+      <translation type="unfinished">Signaling Address</translation>
+    </message>
+    <message>
+      <source>Signaling Port</source>
+      <translation type="unfinished">Signaling Port</translation>
+    </message>
+    <message>
+      <source>Enable UPnP</source>
+      <translation type="unfinished">Enable UPnP</translation>
+    </message>
   </context>
   <context>
     <name>SettingsDialogHelperTexts</name>
@@ -3789,6 +3825,54 @@ Se eliminarán todos los ajustes específicos para cada juego.</translation>
     <message>
       <source>&amp;Open User Directory</source>
       <translation>&amp;Abrir directorio de usuarios</translation>
+    </message>
+    <message>
+      <source>Shad&amp;Net...</source>
+      <translation type="unfinished">Shad&amp;Net...</translation>
+    </message>
+    <message>
+      <source>On</source>
+      <translation type="unfinished">On</translation>
+    </message>
+    <message>
+      <source>On (%1)</source>
+      <translation type="unfinished">On (%1)</translation>
+    </message>
+    <message>
+      <source>Off</source>
+      <translation type="unfinished">Off</translation>
+    </message>
+    <message>
+      <source>ShadNet Settings - %1</source>
+      <translation type="unfinished">ShadNet Settings - %1</translation>
+    </message>
+    <message>
+      <source>Enable ShadNet for this user</source>
+      <translation type="unfinished">Enable ShadNet for this user</translation>
+    </message>
+    <message>
+      <source>Account ID (NPID)</source>
+      <translation type="unfinished">Account ID (NPID)</translation>
+    </message>
+    <message>
+      <source>Password</source>
+      <translation type="unfinished">Password</translation>
+    </message>
+    <message>
+      <source>Show password</source>
+      <translation type="unfinished">Show password</translation>
+    </message>
+    <message>
+      <source>Account ID (NPID):</source>
+      <translation type="unfinished">Account ID (NPID):</translation>
+    </message>
+    <message>
+      <source>Password:</source>
+      <translation type="unfinished">Password:</translation>
+    </message>
+    <message>
+      <source>Shad&amp;Net Settings...</source>
+      <translation type="unfinished">Shad&amp;Net Settings...</translation>
     </message>
   </context>
   <context>
