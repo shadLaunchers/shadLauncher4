40e6f6ead05848a1dacc030e53466e97acc9d2a4
georgemoralis <4313123+georgemoralis@users.noreply.github.com>
2026-08-01 03:32:09 +0000

    [ci skip] Qt GUI: Update Translation.

--- src/qt_ui/translations/en_US.ts
+++ src/qt_ui/translations/en_US.ts
@@ -1450,6 +1450,65 @@ It will be re-read from disk on the next refresh.</source>
         <source>Clear &amp;Metadata Cache</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Convert to ZArchive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This game is already packed as a ZArchive.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This game&apos;s folder could not be found on disk.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Convert %1 to ZArchive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ZArchive Files (*.zar)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 already exists. Overwrite it?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This will pack &quot;%1&quot; into a single read-only .zar archive. Depending on the game&apos;s size this can take a while, and the archive will temporarily need as much free disk space as the game itself.
+
+The original folder is left untouched until conversion succeeds,you&apos;ll be asked afterward whether to delete it.
+
+Continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Packing %1...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to convert game to ZArchive:
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Conversion finished. Delete the original game folder now to free up disk space?
+
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The archive was created, but the original folder could not be fully deleted. You can remove it manually.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Packing: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Convert to ZArchive (.zar)...</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>GameListGrid</name>
