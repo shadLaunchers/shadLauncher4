55bd630cc54eef07a876f404f24db6d01053f885
rainmakerv2 <30595646+rainmakerv3@users.noreply.github.com>
2026-08-08 07:05:57 +0800

    fix trophies/shortcuts with zar

--- src/qt_ui/trophy_viewer.cpp
+++ src/qt_ui/trophy_viewer.cpp
@@ -15,6 +15,7 @@
 #include "common/path_util.h"
 #include "core/emulator_settings.h"
 #include "core/file_format/npbind.h"
+#include "core/file_sys/game_backend.h"
 #include "core/user_settings.h"
 #include "trophy_viewer.h"
 
@@ -180,6 +181,10 @@ TrophyViewer::TrophyViewer(std::shared_ptr<GUISettings> gui_settings, QString tr
     gameTrpPath_ = gameTrpPath;
     std::filesystem::path npbindPath =
         Common::FS::PathFromQString(gameTrpPath) / "sce_sys" / "npbind.dat";
+    if (const auto resolved = Core::FileSys::ResolveGameFilePath(
+            Common::FS::PathFromQString(gameTrpPath), "sce_sys/npbind.dat")) {
+        npbindPath = *resolved;
+    }
 
     NPBindFile npbind;
     if (!npbind.Load(npbindPath.string())) {
