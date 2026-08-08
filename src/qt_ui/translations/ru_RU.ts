a74c519de76e68968e4472d0a4c7919b5bec7f54
georgemoralis <giorgosmrls@gmail.com>
2026-08-08 09:15:16 +0300

    New translations en_us.ts (Russian)
    [ci skip]

--- src/qt_ui/translations/ru_RU.ts
+++ src/qt_ui/translations/ru_RU.ts
@@ -2802,6 +2802,24 @@ Select an emulator version from the right panel.</source>
       <source>Audio</source>
       <translation>Аудио</translation>
     </message>
+    <message numerus="yes">
+      <source>%n game(s)</source>
+      <translation type="unfinished">
+        <numerusform>%n game(s)</numerusform>
+        <numerusform>%n game(s)</numerusform>
+        <numerusform>%n game(s)</numerusform>
+        <numerusform>%n game(s)</numerusform>
+      </translation>
+    </message>
+    <message numerus="yes">
+      <source>%1 of %n game(s)</source>
+      <translation type="unfinished">
+        <numerusform>%1 of %n game(s)</numerusform>
+        <numerusform>%1 of %n game(s)</numerusform>
+        <numerusform>%1 of %n game(s)</numerusform>
+        <numerusform>%1 of %n game(s)</numerusform>
+      </translation>
+    </message>
   </context>
   <context>
     <name>NpBindDialog</name>
@@ -3945,6 +3963,14 @@ This will remove all game-specific overrides.</source>
     <message>
       <source>Signaling Info</source>
       <translation>Информация про сигнал</translation>
+    </message>
+    <message>
+      <source>Red Zone Protection</source>
+      <translation type="unfinished">Red Zone Protection</translation>
+    </message>
+    <message>
+      <source>Static Patching</source>
+      <translation type="unfinished">Static Patching</translation>
     </message>
   </context>
   <context>
