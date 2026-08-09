8db9931c10c065f1a2c86275915591f877fec73c
georgemoralis <giorgosmrls@gmail.com>
2026-08-09 22:36:28 +0300

    New translations en_us.ts (Chinese Simplified)
    [ci skip]

--- src/qt_ui/translations/zh_CN.ts
+++ src/qt_ui/translations/zh_CN.ts
@@ -1499,6 +1499,14 @@ Continue?</translation>
       <source>Edit Tooltip Notes</source>
       <translation type="unfinished">Edit Tooltip Notes</translation>
     </message>
+    <message>
+      <source>The archive was saved to a folder that isn&apos;t one of your configured game folders, so it won&apos;t show up in the game list. Move it into a configured folder, or add this folder under Settings, if you&apos;d like it to appear.</source>
+      <translation type="unfinished">The archive was saved to a folder that isn&apos;t one of your configured game folders, so it won&apos;t show up in the game list. Move it into a configured folder, or add this folder under Settings, if you&apos;d like it to appear.</translation>
+    </message>
+    <message>
+      <source>The folder was extracted, but it isn&apos;t inside one of your configured game folders, so it won&apos;t show up in the game list. Move it into a configured folder, or add this folder under Settings, if you&apos;d like it to appear.</source>
+      <translation type="unfinished">The folder was extracted, but it isn&apos;t inside one of your configured game folders, so it won&apos;t show up in the game list. Move it into a configured folder, or add this folder under Settings, if you&apos;d like it to appear.</translation>
+    </message>
   </context>
   <context>
     <name>GameListFrame</name>
