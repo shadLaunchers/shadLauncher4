5ebc879c90084232fdf9f0e72c6a6d35a647243e
georgemoralis <giorgosmrls@gmail.com>
2026-08-18 12:58:12 +0300

    New Crowdin updates (#179)
    * New translations en_us.ts (German)
    
    [ci skip]
    
    * New translations en_us.ts (Portuguese, Brazilian)
    
    [ci skip]
    
    * New translations en_us.ts (Romanian)
    
    [ci skip]
    
    * New translations en_us.ts (French)
    
    [ci skip]
    
    * New translations en_us.ts (Spanish)
    
    [ci skip]
    
    * New translations en_us.ts (Arabic)
    
    [ci skip]
    
    * New translations en_us.ts (Czech)
    
    [ci skip]
    
    * New translations en_us.ts (Danish)
    
    [ci skip]
    
    * New translations en_us.ts (Greek)
    
    [ci skip]
    
    * New translations en_us.ts (Finnish)
    
    [ci skip]
    
    * New translations en_us.ts (Hungarian)
    
    [ci skip]
    
    * New translations en_us.ts (Italian)
    
    [ci skip]
    
    * New translations en_us.ts (Japanese)
    
    [ci skip]
    
    * New translations en_us.ts (Korean)
    
    [ci skip]
    
    * New translations en_us.ts (Dutch)
    
    [ci skip]
    
    * New translations en_us.ts (Norwegian)
    
    [ci skip]
    
    * New translations en_us.ts (Polish)
    
    [ci skip]
    
    * New translations en_us.ts (Portuguese)
    
    [ci skip]
    
    * New translations en_us.ts (Russian)
    
    [ci skip]
    
    * New translations en_us.ts (Swedish)
    
    [ci skip]
    
    * New translations en_us.ts (Turkish)
    
    [ci skip]
    
    * New translations en_us.ts (Ukrainian)
    
    [ci skip]
    
    * New translations en_us.ts (Chinese Simplified)
    
    [ci skip]
    
    * New translations en_us.ts (Chinese Traditional)
    
    [ci skip]
    
    * New translations en_us.ts (Vietnamese)
    
    [ci skip]
    
    * New translations en_us.ts (Indonesian)
    
    [ci skip]
    
    * New translations en_us.ts (Thai)
    
    [ci skip]
    
    * New translations en_us.ts (Croatian)
    
    [ci skip]
    
    * New translations en_us.ts (Norwegian Nynorsk)
    
    [ci skip]
    
    * New translations en_us.ts (Norwegian Bokmal)
    
    [ci skip]

--- src/qt_ui/translations/es_ES.ts
+++ src/qt_ui/translations/es_ES.ts
@@ -1510,6 +1510,92 @@ El archivo .zar original no se alterará hasta que la conversión haya terminado
       <source>The folder was extracted, but it isn&apos;t inside one of your configured game folders, so it won&apos;t show up in the game list. Move it into a configured folder, or add this folder under Settings, if you&apos;d like it to appear.</source>
       <translation>Se ha extraído la carpeta, pero no es una de las carpetas de juegos que tienes configuradas, así que no se mostrará en la lista de juegos. Llévalo a una carpeta que sí esté configurada o, si quieres que esta carpeta nueva aparezca, añádela desde dentro de la configuración.</translation>
     </message>
+    <message>
+      <source>Delete Game</source>
+      <translation type="unfinished">Delete Game</translation>
+    </message>
+    <message>
+      <source>Are you sure you want to delete %1&apos;s game directory?
+
+Its update/patch directory will be deleted too.</source>
+      <translation type="unfinished">Are you sure you want to delete %1&apos;s game directory?
+
+Its update/patch directory will be deleted too.</translation>
+    </message>
+    <message>
+      <source>This game has no saved trophies to delete!</source>
+      <translation type="unfinished">This game has no saved trophies to delete!</translation>
+    </message>
+    <message>
+      <source>Delete Trophy</source>
+      <translation type="unfinished">Delete Trophy</translation>
+    </message>
+    <message>
+      <source>Are you sure you want to delete %1&apos;s trophy data?
+
+This removes the unpacked trophy files and the unlocked trophies of every user. Any other copy of this game shares the same trophy data and will lose it too. This cannot be undone.</source>
+      <translation type="unfinished">Are you sure you want to delete %1&apos;s trophy data?
+
+This removes the unpacked trophy files and the unlocked trophies of every user. Any other copy of this game shares the same trophy data and will lose it too. This cannot be undone.</translation>
+    </message>
+    <message>
+      <source>Some trophy data could not be deleted.</source>
+      <translation type="unfinished">Some trophy data could not be deleted.</translation>
+    </message>
+    <message>
+      <source>Trophy</source>
+      <translation type="unfinished">Trophy</translation>
+    </message>
+    <message>
+      <source>Trophy data deleted successfully.</source>
+      <translation type="unfinished">Trophy data deleted successfully.</translation>
+    </message>
+    <message>
+      <source>&amp;Rename in Game List...</source>
+      <translation type="unfinished">&amp;Rename in Game List...</translation>
+    </message>
+    <message>
+      <source>&amp;Categories</source>
+      <translation type="unfinished">&amp;Categories</translation>
+    </message>
+    <message>
+      <source>&amp;Move From &quot;%1&quot; To</source>
+      <translation type="unfinished">&amp;Move From &quot;%1&quot; To</translation>
+    </message>
+    <message>
+      <source>&amp;Move To</source>
+      <translation type="unfinished">&amp;Move To</translation>
+    </message>
+    <message>
+      <source>Take %1 out of &quot;%2&quot; and put it in &quot;%3&quot;.</source>
+      <translation type="unfinished">Take %1 out of &quot;%2&quot; and put it in &quot;%3&quot;.</translation>
+    </message>
+    <message>
+      <source>Put %1 in &quot;%2&quot; only, removing it from its other categories.</source>
+      <translation type="unfinished">Put %1 in &quot;%2&quot; only, removing it from its other categories.</translation>
+    </message>
+    <message>
+      <source>&amp;New Category...</source>
+      <translation type="unfinished">&amp;New Category...</translation>
+    </message>
+    <message>
+      <source>&amp;Out Of &quot;%1&quot;</source>
+      <translation type="unfinished">&amp;Out Of &quot;%1&quot;</translation>
+    </message>
+    <message>
+      <source>&amp;No Category</source>
+      <translation type="unfinished">&amp;No Category</translation>
+    </message>
+    <message>
+      <source>Rename in Game List</source>
+      <translation type="unfinished">Rename in Game List</translation>
+    </message>
+    <message>
+      <source>Shown instead of &quot;%1&quot; [%2].
+Leave empty to use the original name.</source>
+      <translation type="unfinished">Shown instead of &quot;%1&quot; [%2].
+Leave empty to use the original name.</translation>
+    </message>
   </context>
   <context>
     <name>GameListFrame</name>
@@ -1655,12 +1741,66 @@ Se va a utilizar la base de datos local.
       <source>Failed to convert icon.</source>
       <translation>Error al convertir el icono.</translation>
     </message>
-  </context>
-  <context>
-    <name>GameListGrid</name>
     <message>
-      <source>Notes:</source>
-      <translation>Notas:</translation>
+      <source>Right click a tab to add, rename or delete a category.
+Use the game&apos;s right click menu to put it into a category.</source>
+      <translation type="unfinished">Right click a tab to add, rename or delete a category.
+Use the game&apos;s right click menu to put it into a category.</translation>
+    </message>
+    <message>
+      <source>All</source>
+      <translation type="unfinished">All</translation>
+    </message>
+    <message>
+      <source>Every game found in your game folders</source>
+      <translation type="unfinished">Every game found in your game folders</translation>
+    </message>
+    <message numerus="yes">
+      <source>%n game(s) in this category</source>
+      <translation type="unfinished">
+        <numerusform>%n game(s) in this category</numerusform>
+        <numerusform>%n game(s) in this category</numerusform>
+      </translation>
+    </message>
+    <message>
+      <source>New Category</source>
+      <translation type="unfinished">New Category</translation>
+    </message>
+    <message>
+      <source>Category name:</source>
+      <translation type="unfinished">Category name:</translation>
+    </message>
+    <message>
+      <source>Category Already Exists</source>
+      <translation type="unfinished">Category Already Exists</translation>
+    </message>
+    <message>
+      <source>A category named &quot;%1&quot; already exists.</source>
+      <translation type="unfinished">A category named &quot;%1&quot; already exists.</translation>
+    </message>
+    <message>
+      <source>&amp;New Category...</source>
+      <translation type="unfinished">&amp;New Category...</translation>
+    </message>
+    <message>
+      <source>&amp;Rename &quot;%1&quot;</source>
+      <translation type="unfinished">&amp;Rename &quot;%1&quot;</translation>
+    </message>
+    <message>
+      <source>&amp;Delete &quot;%1&quot;</source>
+      <translation type="unfinished">&amp;Delete &quot;%1&quot;</translation>
+    </message>
+    <message>
+      <source>Rename Category</source>
+      <translation type="unfinished">Rename Category</translation>
+    </message>
+    <message>
+      <source>Delete Category</source>
+      <translation type="unfinished">Delete Category</translation>
+    </message>
+    <message>
+      <source>Delete the category &quot;%1&quot;? The games in it are not touched.</source>
+      <translation type="unfinished">Delete the category &quot;%1&quot;? The games in it are not touched.</translation>
     </message>
   </context>
   <context>
@@ -1696,10 +1836,6 @@ Se va a utilizar la base de datos local.
     <message>
       <source>Last updated</source>
       <translation>Última actualización</translation>
-    </message>
-    <message>
-      <source>Notes:</source>
-      <translation>Notas:</translation>
     </message>
   </context>
   <context>
@@ -2816,6 +2952,25 @@ Selecciona una versión del emulador en el panel derecho.</translation>
         <numerusform>%1 de %n juegos</numerusform>
       </translation>
     </message>
+    <message>
+      <source>Reset All Custom Titles</source>
+      <translation type="unfinished">Reset All Custom Titles</translation>
+    </message>
+    <message>
+      <source>Restores the original name of every game you renamed in the game list</source>
+      <translation type="unfinished">Restores the original name of every game you renamed in the game list</translation>
+    </message>
+    <message>
+      <source>No game has been renamed.</source>
+      <translation type="unfinished">No game has been renamed.</translation>
+    </message>
+    <message numerus="yes">
+      <source>Restore the original name of %n renamed game(s)?</source>
+      <translation type="unfinished">
+        <numerusform>Restore the original name of %n renamed game(s)?</numerusform>
+        <numerusform>Restore the original name of %n renamed game(s)?</numerusform>
+      </translation>
+    </message>
   </context>
   <context>
     <name>NpBindDialog</name>
@@ -3125,6 +3280,14 @@ Selecciona una versión del emulador en el panel derecho.</translation>
     <message>
       <source>Extracting: %1</source>
       <translation>Extrayendo: %1</translation>
+    </message>
+    <message>
+      <source>Original name: %1</source>
+      <translation type="unfinished">Original name: %1</translation>
+    </message>
+    <message>
+      <source>Notes:</source>
+      <translation type="unfinished">Notes:</translation>
     </message>
   </context>
   <context>
