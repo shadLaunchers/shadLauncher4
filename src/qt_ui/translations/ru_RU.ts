88bccc5bfc9ea491e501be31241bc9b7c2ac0c3c
georgemoralis <giorgosmrls@gmail.com>
2026-09-19 10:53:28 +0300

    New translations en_us.ts (Russian)
    [ci skip]

--- src/qt_ui/translations/ru_RU.ts
+++ src/qt_ui/translations/ru_RU.ts
@@ -443,6 +443,14 @@
       <source>Failed to start update process.</source>
       <translation>Не удалось запустить процесс обновления.</translation>
     </message>
+    <message>
+      <source>Automatic updates on Linux require running the AppImage.</source>
+      <translation type="unfinished">Automatic updates on Linux require running the AppImage.</translation>
+    </message>
+    <message>
+      <source>The AppImage directory is not writable or the AppImage is missing.</source>
+      <translation type="unfinished">The AppImage directory is not writable or the AppImage is missing.</translation>
+    </message>
   </context>
   <context>
     <name>ControlSettings</name>
